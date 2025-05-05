df3faccc08cabc933cd625462a48d507c7ac386b
a-apruett <aapruett@csus.edu>
2025-05-05 14:43:18 -0700

    part one

--- encrypt-it.js
+++ encrypt-it.js
@@ -10,13 +10,13 @@
    * on the page. When this event occurs, the attached function (init) will be called.
    */
   window.addEventListener("load", init);
-
   /**
    * TODO: Write a function comment using JSDoc.
    */
   function init() {
     // Note: In this function, we usually want to set up our event handlers
     // for UI elements on the page.
+    console.log("Window loaded!");
   }
 
   // Add any other functions in this area (you should not implement your
